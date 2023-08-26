90e766bc8aeffbc162292b7f6c8c285ad2eeaed3
MGalaCyber1274 <ibmanggalaputra1274@gmail.com>
2023-08-26 15:37:21 +0000

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # betterlogs.js
 Upgrade your terminal logs with betterlogs.js
+
+```
+npm install discord.js
+yarn add discord.js
+pnpm add discord.js
+```
+
+```
+const betterlogs = require("betterlogs.js");
+inport betterlogs from betterlogs.js
+```
+
+```js
+betterlogs.warn("Hello Word");
+betterlogs.danger("Hello Word");
+betterlogs.info("Hello Word");
+betterlogs.error("Hello Word");
+betterlogs.debug("Hello Word");
+betterlogs.ready("Hello Word");
+betterlogs.database("Hello Word");
+betterlogs.event("Hello Word");
+betterlogs.heartbeat("Hello Word");
+betterlogs.shard("Hello Word");
+betterlogs.cluster("Hello Word");
+betterlogs.command("Hello Word");
+betterlogs.notice("Hello Word");
+betterlogs.critical("Hello Word");
+betterlogs.trace("Hello Word");
+```
---
+++ index.d.ts
@@ -0,0 +1,60 @@
+
+export const warn = (messages: string) => {
+    return messages
+};
+
+export const danger = (messages: string) => {
+    return messages;
+};
+
+export const info = (messages: string) => {
+    return messages;
+};
+
+export const error = (messages: string) => {
+    return messages;
+};
+
+export const debug = (messages: string) => {
+    return messages;
+};
+
+export const ready = (messages: string) => {
+    return messages;
+};
+
+export const database = (messages: string) => {
+    return messages;
+};
+
+export const event = (messages: string) => {
+    return messages;
+};
+
+export const heartbeat = (messages: string) => {
+    return messages;
+};
+
+export const shard = (messages: string) => {
+    return messages;
+};
+
+export const cluster = (messages: string) => {
+    return messages;
+};
+
+export const command = (messages: string) => {
+    return messages;
+};
+
+export const notice = (messages: string) => {
+    return messages;
+};
+
+export const critical = (messages: string) => {
+    return messages;
+};
+
+export const trace = (messages: string) => {
+    return messages;
+};
