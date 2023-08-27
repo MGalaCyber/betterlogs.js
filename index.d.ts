40d36fc82b8cdb4d5ae7dbf803a75f3632e1a851
MGalaCyber1274 <ibmanggalaputra1274@gmail.com>
2023-08-27 08:54:16 +0000

    add options & update v1.2.3

--- index.d.ts
+++ index.d.ts
@@ -1,60 +1,179 @@
-
-export const warn = (messages: string) => {
-    return messages
+/**
+ * Execute a warning action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const warn = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const danger = (messages: string) => {
-    return messages;
+/**
+ * Execute a danger action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const danger = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const info = (messages: string) => {
-    return messages;
+/**
+ * Execute a info action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const info = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const error = (messages: string) => {
-    return messages;
+/**
+ * Execute a error action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const error = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const debug = (messages: string) => {
-    return messages;
+/**
+ * Execute a debug action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const debug = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const ready = (messages: string) => {
-    return messages;
+/**
+ * Execute a ready action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const ready = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const database = (messages: string) => {
-    return messages;
+/**
+ * Execute a database action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const database = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const event = (messages: string) => {
-    return messages;
+/**
+ * Execute a event action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const event = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const heartbeat = (messages: string) => {
-    return messages;
+/**
+ * Execute a heartbeat action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const heartbeat = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const shard = (messages: string) => {
-    return messages;
+/**
+ * Execute a shard action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const shard = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const cluster = (messages: string) => {
-    return messages;
+/**
+ * Execute a cluster action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const cluster = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const command = (messages: string) => {
-    return messages;
+/**
+ * Execute a command action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const command = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const notice = (messages: string) => {
-    return messages;
+/**
+ * Execute a notice action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const notice = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const critical = (messages: string) => {
-    return messages;
+/**
+ * Execute a critical action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const critical = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
 
-export const trace = (messages: string) => {
-    return messages;
+/**
+ * Execute a trace action based on the selected option.
+ *
+ * @param {("1" | "2")} options - The chosen option ("1" or "2") to determine the action.
+ * @param {string} messages - The input message to be displayed in the terminal.
+ * @returns {string} The result of the chosen action or an indication of an invalid option.
+ */
+export const trace = (options: "1" | "2", messages: string) => {
+    if (options === "1") return messages;
+    if (options === "2") return messages;
 };
